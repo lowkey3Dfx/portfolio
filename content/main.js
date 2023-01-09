17a752711e61f438424f65bd988df023a72b70eb
lowkey3Dfx <Arif.Kesdiren@gmail.com>
2023-01-09 18:10:14 +0100

    Changed heading Name

--- content/main.js
+++ content/main.js
@@ -5,7 +5,7 @@ export const attributes = {
       link: '/#portofolio',
       text: 'My work',
     },
-    heading: "I'm Paco Cu, and I create",
+    heading: "I'm Arif Kesdiren, and I create",
     image: '/img/hero.jpeg',
     secondButton: { icon: 'whatsapp', link: '/#contact', text: 'Contact' },
     social: {
